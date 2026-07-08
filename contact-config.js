9f43fc8635de0df882ef43b25361d8313ae567e6
James Moran <jamesdmoran@gmail.com>
2026-07-07 19:59:49 -0500

    Wire contact form endpoint

--- contact-config.js
+++ contact-config.js
@@ -1,4 +1,4 @@
 window.OUTSIDE_SUPPORT_CONTACT_CONFIG = {
-  endpoint: "",
+  endpoint: "https://script.google.com/macros/s/AKfycbxmJ-zzyNNHaowPQyknYqYn6E0-KOByirscdN-ZhqggW_CSJY8aej0gd8mpkdmwgy6Q/exec",
   recipientEmail: "team@outsidesupport.org"
 };
